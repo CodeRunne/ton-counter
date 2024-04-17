af830fcc6469f807d8b7a7ed7fd4e5aa487b92d5
CodeRunne <etimdaniel08@gmail.com>
2024-04-17 13:44:24 +0100

    changed manifest url

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import App from './App.tsx'
 import './index.css'
 
-const manifestUrl = 'https://CodeRunne.github.io/ton-counter';
+const manifestUrl = 'https://raw.githubusercontent.com/CodeRunne/ton-counter/main/public/manifest.json';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <React.StrictMode>
